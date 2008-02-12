a451302a4dd882b15c0ff9a7f3a3f763b27e54a6
David Zuelke <david.zuelke@bitextender.com>
2008-02-12 21:39:03 +0000

    use skip pattern in example

--- examples/test1.php
+++ examples/test1.php
@@ -4,22 +4,33 @@ error_reporting(E_ALL | E_STRICT);
 
 require('../lib/Grammatista.php');
 
-Grammatista::registerScanner('fs', new GrammatistaScannerFilesystem(array('filesystem.path' => realpath(dirname(__FILE__) . '/test1/'))));
-
-Grammatista::registerParser('agxml', array('class' => 'GrammatistaParserXmlAgaviValidation'));
-Grammatista::registerParser('gettextphp', array('class' => 'GrammatistaParserPhpAgavi'));
-Grammatista::registerParser('agsmarty', array('class' => 'GrammatistaParserPcreSmarty', 'options' => array(
-	'pcre.patterns' => array(
-		'/\{trans(\s+(domain=(["\']?)(?P<domain>(?(3)((?!(?<!\\\\)\3).)*|[^\s\}]+))\3|[^\s\}"\'=]+=[^\s\}"\']+|[^\s\}=]+=(["\']?)(?(6)((?!(?<!\\\\)\6).)*|[^\s\}]+)\6))*\s*\}\s*(?P<subpattern>.+?)\s*\{\/trans\}/ms' => array( // best so far! doesn't handle domains without quotation marks yet
-			'/(\s*\{singular\}(?P<singular_message>.+?)\{\/singular\}|\s*\{plural\}(?P<plural_message>.+?)\{\/plural\})+\s*/s' => true,
-			'/(?P<singular_message>.+)/s' => true,
-		),
+Grammatista::registerScanner('fs', new GrammatistaScannerFilesystem(array(
+	'filesystem.path' => realpath(dirname(__FILE__) . '/test1/'),
+	'filesystem.skip_patterns' => array(
+		'#/\.svn$#',
 	)
 )));
 
+Grammatista::registerParser('agxml', array('class' => 'GrammatistaParserXmlAgaviValidation'));
+Grammatista::registerParser('gettextphp', array('class' => 'GrammatistaParserPhpAgavi'));
+Grammatista::registerParser('agsmarty', array(
+	'class' => 'GrammatistaParserPcreSmarty',
+	'options' => array(
+		'pcre.patterns' => array(
+			'/\{trans(\s+(domain=(["\']?)(?P<domain>(?(3)((?!(?<!\\\\)\3).)*|[^\s\}]+))\3|[^\s\}"\'=]+=[^\s\}"\']+|[^\s\}=]+=(["\']?)(?(6)((?!(?<!\\\\)\6).)*|[^\s\}]+)\6))*\s*\}\s*(?P<subpattern>.+?)\s*\{\/trans\}/ms' => array( // best so far! doesn't handle domains without quotation marks yet
+				'/(\s*\{singular\}(?P<singular_message>.+?)\{\/singular\}|\s*\{plural\}(?P<plural_message>.+?)\{\/plural\})+\s*/s' => true,
+				'/(?P<singular_message>.+)/s' => true,
+			),
+		),
+	),
+));
+
 Grammatista::registerStorage('sqlite', new GrammatistaStoragePdo(array('pdo.dsn' => 'sqlite:' . dirname(__FILE__) . '/' . $_SERVER['REQUEST_TIME'] . '.sqlite')));
 
-Grammatista::registerWriter('pot', new GrammatistaWriterFilePo(array('file.basedir' => dirname(__FILE__) . '/' . $_SERVER['REQUEST_TIME'], 'file.pattern' => '%s.pot')));
+Grammatista::registerWriter('pot', new GrammatistaWriterFilePo(array(
+	'file.basedir' => dirname(__FILE__) . '/' . $_SERVER['REQUEST_TIME'],
+	'file.pattern' => '%s.pot',
+)));
 
 $logger = new GrammatistaLoggerShell();
 Grammatista::registerEventResponder('grammatista.parser.parsed', array($logger, 'log'));
