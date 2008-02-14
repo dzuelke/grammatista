d62772d0509b36f81d44fc7d1c1168b641c250de
David Zuelke <david.zuelke@bitextender.com>
2008-02-14 13:39:42 +0000

    xml optimizations

--- lib/Grammatista/Parser/Xml.class.php
+++ lib/Grammatista/Parser/Xml.class.php
@@ -2,7 +2,7 @@
 
 abstract class GrammatistaParserXml extends GrammatistaParser
 {
-	const XMLNS_GRAMMATISTA_PARSER = 'urn:GrammatistaParserXml';
+	const XMLNS_GRAMMATISTA_PARSER_XML = 'urn:GrammatistaParserXml';
 	const XMLNS_SAXON = 'http://icl.com/saxon';
 	
 	protected $doc;
@@ -29,7 +29,7 @@ abstract class GrammatistaParserXml extends GrammatistaParser
 		$this->doc->loadXML($entity->content);
 		
 		$this->xpath = new DOMXPath($this->doc);
-		$this->xpath->registerNamespace('grammatista', self::XMLNS_GRAMMATISTA_PARSER);
+		$this->xpath->registerNamespace('grammatista', self::XMLNS_GRAMMATISTA_PARSER_XML);
 	}
 	
 	protected function tagElement(DOMElement $element)
@@ -37,21 +37,21 @@ abstract class GrammatistaParserXml extends GrammatistaParser
 		// generate unique tag to flag the element and set it as an attribute
 		// that way we can find it again in XPath and XSL
 		$tag = md5(uniqid());
-		$element->setAttributeNS(self::XMLNS_GRAMMATISTA_PARSER, 'grammatista:tag', $tag);
+		$element->setAttributeNS(self::XMLNS_GRAMMATISTA_PARSER_XML, 'grammatista:tag', $tag);
 		return $tag;
 	}
 	
 	protected function untagElement(DOMElement $element)
 	{
 		// remove the tag attribute set above
-		$element->removeAttributeNS(self::XMLNS_GRAMMATISTA_PARSER, 'grammatista:tag');
+		$element->removeAttributeNS(self::XMLNS_GRAMMATISTA_PARSER_XML, 'grammatista:tag');
 	}
 	
 	protected function findLine($tag, $elementName, array $namespaces = array())
 	{
 		// stylesheet to find that tagged element
 		$xml = '<?xml version="1.0" encoding="utf-8"?>';
-		$xml.= sprintf('<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:grammatista="%1$s" xmlns:saxon="%2$s"', self::XMLNS_GRAMMATISTA_PARSER, self::XMLNS_SAXON);
+		$xml.= sprintf('<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:grammatista="%1$s" xmlns:saxon="%2$s"', self::XMLNS_GRAMMATISTA_PARSER_XML, self::XMLNS_SAXON);
 		foreach($namespaces as $prefix => $uri) {
 			$xml.= sprintf(' xmlns:%s="%s"', $prefix, $uri);
 		}
@@ -70,6 +70,10 @@ abstract class GrammatistaParserXml extends GrammatistaParser
 		
 		// grab the line number by running the stylesheet
 		$line = trim($xslt->transformToXML($this->doc));
+		
+		// cleanup
+		$xslt = null;
+		$xsl = null;
 		
 		return $line;
 	}
