45c6b1015a35298476215f2fa49f872ed64f3771
David Zuelke <david.zuelke@bitextender.com>
2008-02-21 22:27:19 +0000

    Better regular expressions that now also catch domains without quotation marks

--- examples/test1.php
+++ examples/test1.php
@@ -18,10 +18,13 @@ Grammatista::registerParser('agsmarty', array(
 	'options' => array(
 		'pcre.comment_pattern' => '/\{\*\s*%s\s*(?P<comment>[^\*\}]+?)\s*\*\}(?!.*?\{trans[\s\}])/s',
 		'pcre.patterns' => array(
-			'/\{trans(\s+(domain=(["\']?)(?P<domain>(?(3)((?!(?<!\\\\)\3).)*|[^\s\}]+))\3|[^\s\}"\'=]+=[^\s\}"\']+|[^\s\}=]+=(["\']?)(?(6)((?!(?<!\\\\)\6).)*|[^\s\}]+)\6))*\s*\}\s*(?P<subpattern>.+?)\s*\{\/trans\}/ms' => array( // best so far! doesn't handle domains without quotation marks yet
+			// '/\{trans(\s+(domain=(["\']?)(?P<domain>(?(3)((?!(?<!\\\\)\3).)*|[^\s\}]+))\3|[^\s\}"\'=]+=[^\s\}"\']+|[^\s\}=]+=(["\']?)(?(6)((?!(?<!\\\\)\6).)*|[^\s\}]+)\6))*\s*\}\s*(?P<subpattern>.+?)\s*\{\/trans\}/ms' => array( // best so far! doesn't handle domains without quotation marks yet
+			'/\{trans(\s+(domain=(["\']?)(?P<domain>.*?(?(-2)(?=(?<!\\\\)\g{-2})|(?=[\s\}])))(?(-2)\g{-2})|[^\s\}=]+=(["\']?).*?(?(-1)(?=(?<!\\\\)\g{-1})|(?=[\s\}]))\g{-1}))*\s*\}\s*(?P<subpattern>.+?)\s*\{\/trans\}/ms' => array( // best so far! doesn't handle domains without quotation marks yet
 				'/(\s*\{singular\}(?P<singular_message>.+?)\{\/singular\}|\s*\{plural\}(?P<plural_message>.+?)\{\/plural\})+\s*/s' => true,
 				'/(?P<singular_message>.+)/s' => true,
+				
 			),
+			'/[^\s\}=]+=(["\'])?(?P<singular_message>.*?(?(-2)(?=(?<!\\\\)\g{-2})|(?=[\s\}\|])))(?(-2)\g{-2})\|trans(\:(["\'])?(?P<domain>.*?(?(-2)(?=(?<!\\\\)\g{-2})|(?=[\s\}"\'\:])))(?(-2)\g{-2}))?/' => true,
 		),
 	),
 ));
--- examples/test1/lol.tpl
+++ examples/test1/lol.tpl
@@ -1,10 +1,12 @@
 Smarty lolz :)
 
 {* tc: comment test *}
-{trans domain="products.latest"}hello!{/trans}
+{trans domain=products.latest}hello!{/trans}
 {trans zomg="bar" domain="newsletter.signup"  }Your E-Mail Address{/trans}
 
-{trans domain="widget.messaging" arg1=$messageCount}{singular}You have one new Message{/singular}
+{foo bar=baz|trans:sdfghjsd:"en_US"}
+
+{trans arg1=$messageCount domain="widget.messaging" arg2="some text with \" escaped quotation marks :D"}{singular}You have one new Message{/singular}
 {plural}You have %s new Messages{/plural}{/trans}
 
 {*tc:comment with following non whitespace chars*}<input type="text" name="foo" value="{trans domain="products.latest"}hello again!{/trans}" />
