fd341015afd4d679ab62d83b74f8680ee03447a2
Gi7mo! <github@gi7mo.org>
2013-11-05 13:01:09 +0100

    add support for agavi 1.1 xml namespaces

--- lib/Grammatista/Parser/Xml/Agavi.class.php
+++ lib/Grammatista/Parser/Xml/Agavi.class.php
@@ -4,10 +4,12 @@ abstract class GrammatistaParserXmlAgavi extends GrammatistaParserXml
 {
 	const XMLNS_AGAVI_ENVELOPE_0_11 = 'http://agavi.org/agavi/1.0/config';
 	const XMLNS_AGAVI_ENVELOPE_1_0 = 'http://agavi.org/agavi/config/global/envelope/1.0';
+	const XMLNS_AGAVI_ENVELOPE_1_1 = 'http://agavi.org/agavi/config/global/envelope/1.1';
 	
 	static $envelopeNamespaces = array(
 		self::XMLNS_AGAVI_ENVELOPE_0_11,
 		self::XMLNS_AGAVI_ENVELOPE_1_0,
+		self::XMLNS_AGAVI_ENVELOPE_1_1,
 	);
 	
 	protected function load(GrammatistaEntity $entity)
@@ -16,6 +18,7 @@ abstract class GrammatistaParserXmlAgavi extends GrammatistaParserXml
 		
 		$this->xpath->registerNamespace('agavi_envelope_0_11', self::XMLNS_AGAVI_ENVELOPE_0_11);
 		$this->xpath->registerNamespace('agavi_envelope_1_0', self::XMLNS_AGAVI_ENVELOPE_1_0);
+		$this->xpath->registerNamespace('agavi_envelope_1_1', self::XMLNS_AGAVI_ENVELOPE_1_1);
 	}
 	
 	public function handles(GrammatistaEntity $entity)
